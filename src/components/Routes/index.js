324bc17e0ed872afc7503dedd1ac40d8881b6425
Kendra1 <ksydfl@gmail.com>
2020-09-12 16:22:24 +0200

    Added public and private routes

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
 
-// import PrivateRoute from '../../containers/PrivateRoute'
-// import PublicRoute from '../../containers/PublicRoute'
+import PrivateRoute from '../../containers/PrivateRoute'
+import PublicRoute from '../../containers/PublicRoute'
 import Login from '../../containers/auth/Login'
-// import Dashboard from '../../containers/Dashboard'
+import Dashboard from '../../containers/Dashboard'
 
 import { LOGIN, DASHBOARD } from '../../routes'
 
@@ -14,17 +14,12 @@ function Routes(props) {
   return (
     <BrowserRouter>
       <Switch>
-        <Route restricted={true} component={Login} path={LOGIN} exact />
+        <PublicRoute restricted={true} component={Login} path={LOGIN} exact />
+        <PrivateRoute component={Dashboard} path={DASHBOARD} />
       </Switch>
     </BrowserRouter>
   );
 }
 
-const mapStateToProps = state => {
-    return {
-    };
-  };
 
-const withConnect = connect(mapStateToProps);
-
-export default compose(withConnect)(Routes);
+export default Routes;
---
+++ src/containers/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function Dashboard(props) {
+    return (
+        <div>da</div>
+    )
+}
+
+export default Dashboard;
